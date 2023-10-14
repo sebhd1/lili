57f4a64bde438868e769d1b66cb48fd5578dd716
Sebastiano Ballotta <sebeballotta@outlook.com>
2023-10-14 19:26:54 +0200

    Created Seeders

---
+++ database/seeders/PassengerSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Passenger;
+use Illuminate\Database\Seeder;
+
+class PassengerSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Passenger::factory(5)->create();
+    }
+}
