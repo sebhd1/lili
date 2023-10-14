1bcc100e052a32feec04071ac988da33190a72be
Sebastiano Ballotta <sebeballotta@outlook.com>
2023-10-14 19:27:21 +0200

    Added Blueprint macro for fullName

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +12,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        Blueprint::macro('fullName', fn(string $column = 'full_name') => $this->string($column)
+            ->virtualAs("TRIM(CONCAT(IFNULL(name, ''), ' ', IFNULL(surname, '')))")
+    );
     }
 
     /**
